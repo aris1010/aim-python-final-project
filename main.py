ca7b8f5b61dcab2b6887e6688a9bc94069aeccd6
Aristide <acamara@mail.mccneb.edu>
2024-07-16 20:01:11 -0500

    Update 1
    Update 1

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+class Character:
+    def __init__(self, name, health, energy, experience, level):
+        self.name = name
+        self.health = health
+        self.energy = energy
+        self.experience = experience
+        self.level = level
